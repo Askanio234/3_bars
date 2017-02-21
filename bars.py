5cabb2ded3225a8c7c122e657462104767a82656
Artem Chernyaev <askanio234@gmail.com>
2017-02-21 22:10:18 +0300

    refactured after wallie remarks

--- bars.py
+++ bars.py
@@ -1,11 +1,13 @@
 import json
+import os
 from geopy.distance import vincenty
 
 
 def load_data(filepath):
+    if not os.path.exists(filepath):
+        return None
     with open(filepath, "r") as json_file:
-        data = json.load(json_file)
-    return data
+        return json.load(json_file)
 
 
 def get_biggest_bar(data):
@@ -26,14 +28,29 @@ def get_closest_bar(data, longitude, latitude):
 
 if __name__ == '__main__':
     filepath = input("Введите путь до файла: ")
-    data = load_data(filepath)
-    biggest_bar = get_biggest_bar(data)
-    smallest_bar = get_smallest_bar(data)
-    print("самый большрй бар '{}' и в нем {} мест".format(
-        biggest_bar["Name"], biggest_bar["SeatsCount"]))
-    print("самый маленький бар '{}' и в нем {} мест".format(
-        smallest_bar["Name"], smallest_bar["SeatsCount"]))
-    latitude = float(input("Введите вашу широту: "))
-    longitude = float(input("Введите вашу долготу: "))
-    closest_bar = get_closest_bar(data, longitude, latitude)
-    print("К вам ближе всего бар '{}'".format(closest_bar["Name"]))
+    data_from_json = load_data(filepath)
+    if data_from_json is None:
+        print("Некорректный путь до файла")
+    else:
+        biggest_bar = get_biggest_bar(data_from_json)
+        smallest_bar = get_smallest_bar(data_from_json)
+        print("самый большрй бар '{}' и в нем {} мест".format(
+            biggest_bar["Name"], biggest_bar["SeatsCount"]))
+        print("самый маленький бар '{}' и в нем {} мест".format(
+            smallest_bar["Name"], smallest_bar["SeatsCount"]))
+
+        try:
+            latitude = float(input("Введите вашу широту: "))
+        except ValueError:
+            latitude = None
+
+        try:
+            longitude = float(input("Введите вашу долготу: "))
+        except ValueError:
+            longitude = None
+
+        if longitude is None or latitude is None:
+            print("Некорректные координаты")
+        else:
+            closest_bar = get_closest_bar(data_from_json, longitude, latitude)
+            print("К вам ближе всего бар '{}'".format(closest_bar["Name"]))
